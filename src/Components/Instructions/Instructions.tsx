f3a6ac813a883ce33be753be1b479cdbf0a5597b
bkitano <briankitano@gmail.com>
2023-06-29 15:00:51 -0400

    adding second page

--- src/Components/Instructions/Instructions.tsx
+++ src/Components/Instructions/Instructions.tsx
@@ -1,15 +1,23 @@
 import { usePage } from "../PageContext";
 import { Introduction } from "./lessons/Introduction";
+import { ProblemDescription } from "./lessons/ProblemDescription";
 
 const Instructions = () => {
   const { page } = usePage();
   const LessonsMap: Record<number, JSX.Element> = {
     0: <Introduction />,
+    1: <ProblemDescription />,
   };
 
-  return <div style={{
-    width: "90%",
-  }}>{LessonsMap[page]}</div>;
+  return (
+    <div
+      style={{
+        width: "90%",
+      }}
+    >
+      {LessonsMap[page]}
+    </div>
+  );
 };
 
 export { Instructions };
---
+++ src/Components/Instructions/lessons/ProblemDescription.tsx
@@ -0,0 +1,15 @@
+import { Typography } from "@mui/material";
+
+const ProblemDescription = () => {
+  return (
+    <>
+      <Typography variant="h4" gutterBottom>
+        The Problem: "Is it legal?"
+      </Typography>
+      <br />
+      <Typography></Typography>
+    </>
+  );
+};
+
+export { ProblemDescription };
