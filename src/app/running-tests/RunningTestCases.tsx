a6580ceb75a736af114bc007e1b31385a0922003
bkitano <briankitano@gmail.com>
2023-06-30 19:26:42 -0400

    working batching

--- src/app/running-tests/RunningTestCases.tsx
+++ src/app/running-tests/RunningTestCases.tsx
@@ -15,80 +15,8 @@ const RunningTestCases = () => (
       Running Test Cases
     </Typography>
     <Typography variant="h5">
-      We connect our prompt to our test cases with curly brackets.
+      We can run all the test cases at the same time using prompt variables.
     </Typography>
-    <br />
-    <Typography variant="body1">
-      When you put a variable in{" "}
-      <span style={{ fontWeight: "bold" }}>curly brackets</span>, the prompt
-      will fill it with the variable value. For example, if you have a prompt
-      like this:
-    </Typography>
-    <br />
-    <div
-      style={{
-        padding: "15px",
-        backgroundColor: "#282c34",
-        border: "2px solid white",
-        borderRadius: "5px",
-      }}
-    >
-      <Typography
-        style={{
-          fontFamily: "monospace",
-        }}
-      >
-        Write me a poem about {`{{topic}}`}.
-      </Typography>
-    </div>
-    <br />
-    <Typography>then when you run the prompt with the test case</Typography>
-    <br />
-    <TableContainer component={Card}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>
-              <Typography
-                style={{
-                  fontFamily: "monospace",
-                }}
-              >{`{{topic}}`}</Typography>
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          <TableRow>
-            <TableCell>
-              <Typography>love</Typography>
-            </TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </TableContainer>
-    <br />
-    <Typography>
-      the prompt will fill in the variable and run it like this:
-    </Typography>
-    <br />
-    <div
-      style={{
-        padding: "15px",
-        backgroundColor: "#282c34",
-        border: "2px solid white",
-        borderRadius: "5px",
-      }}
-    >
-      <Typography
-        style={{
-          fontFamily: "monospace",
-        }}
-      >
-        Write me a poem about love.
-      </Typography>
-    </div>
-    <br />
-    <Typography variant="h6">Batching test cases</Typography>
     <br />
     <Typography>
       We can use brackets to run our prompt on multiple test cases, all in one
