2e45e9a2b6dbbe6a0fa4e47befba039eb764e59d
bkitano <briankitano@gmail.com>
2023-07-01 10:01:30 -0400

    adding text copy button, but actually this should just be an editor

--- src/Components/EditorTextExample.tsx
+++ src/Components/EditorTextExample.tsx
@@ -1,23 +1,42 @@
-import { Typography } from "@mui/material";
+import { ContentCopy } from "@mui/icons-material";
+import { IconButton, Typography } from "@mui/material";
 
-const EditorTextExample = ({ children }: { children: string }) => (
-  <div
-    style={{
-      padding: "15px",
-      backgroundColor: "#282c34",
-      border: "2px solid white",
-      borderRadius: "5px",
-      whiteSpace: "pre-wrap",
-    }}
-  >
-    <Typography
+const EditorTextExample = ({ children }: { children: string }) => {
+  return (
+    <div
       style={{
-        fontFamily: "monospace",
+        padding: "15px",
+        backgroundColor: "#282c34",
+        border: "2px solid white",
+        borderRadius: "5px",
+        whiteSpace: "pre-wrap",
       }}
     >
-      {children}
-    </Typography>
-  </div>
-);
+      <Typography
+        style={{
+          fontFamily: "monospace",
+        }}
+      >
+        {children}
+      </Typography>
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "flex-end",
+        }}
+      >
+        <IconButton
+          onClick={() => {
+            navigator.clipboard.writeText(children);
+          }}
+        >
+          <ContentCopy style={{ color: "white" }} />
+        </IconButton>
+      </div>
+    </div>
+  );
+};
 
 export { EditorTextExample };
--- src/app/few-shot/FewShotPrompting.tsx
+++ src/app/few-shot/FewShotPrompting.tsx
@@ -1,19 +1,49 @@
+import { EditorTextExample } from "@/Components/EditorTextExample";
 import { Typography } from "@mui/material";
 
 export const FewShotPrompting = () => (
   <>
     <Typography variant="h4" gutterBottom>
-      Providing Examples - Few-shot prompting
+      Few-shot prompting
     </Typography>
     <Typography variant="h5">
-      Guide your LLM by showing it examples of what you want.
+      Guide your LLM by providing it examples of what you want in the prompt.
     </Typography>
     <br />
     <Typography variant="body1">
       <span style={{ fontWeight: "bold" }}>Few-shot prompting</span> is a
       technique where we provide the LLM with a few examples of the task we want
-      it to perform. It's like giving it a little nudge in the right direction.
+      it to perform, called <span style={{ fontWeight: "bold" }}>shots</span>.
+      It's like giving it a little nudge in the right direction.
     </Typography>
+    <br />
+    <Typography>
+      For our poetry example, we can provide the LLM with a few examples of the
+      kind of poems we want it to write. We can do this by adding shots to our
+      prompt.
+    </Typography>
+    <br />
+    <EditorTextExample>
+      {`INSTRUCTIONS:
+Write me a haiku about {{topic}}.
+
+FORMAT:
+\`\`\`xml
+<poem>
+    <line>LINE</line>
+    <line>LINE</line>
+    <line>LINE</line>
+</poem>
+\`\`\`
+
+EXAMPLES:
+<poem>
+<line>Whispering winds entwine,</line>
+<line>Beneath the pale moonshine,</line>
+<line>Love's tender design.</line>
+</poem>
+`}
+    </EditorTextExample>
     <br />
     <Typography>
       Practice creating a few-shot prompt. Remember, the goal is to make the LLM
